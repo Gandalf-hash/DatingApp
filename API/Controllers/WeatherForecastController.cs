7c6a27616a98b65558b57509806f618a97c4da64
Gandalf-hash <phiritshepiso99@gmail.com>
2023-02-06 08:52:20 +0200

    End of section 4

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ public class WeatherForecastController : ControllerBase
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = "Hello World!"
+            Summary = "Hello World!!"
         })
         .ToArray();
     }

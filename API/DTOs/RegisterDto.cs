b2ed139e9f8b6e459bb14f941f2872961a36ba89
Gandalf-hash <phiritshepiso99@gmail.com>
2023-02-13 12:23:21 +0200

    End of section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,11 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
-        [Required]
+        [Required] public string knownAs { get; set; }
+        [Required] public string gender { get; set; }
+        [Required] public DateOnly? DateOfBirth { get; set; }
+        [Required] public string city { get; set; }
+        [Required] public string country { get; set; }
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
